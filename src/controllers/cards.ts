70aad3a0d00d485e420c236d868dab7a8ca0cd43
tchesa <cesarolimpio@gmail.com>
2021-07-02 00:00:54 -0300

    express types

--- src/controllers/cards.ts
+++ src/controllers/cards.ts
@@ -1,6 +1,7 @@
+import { Request, Response } from 'express';
 import mtgClient from '../modules/mtgClient';
 
-export const getCards = async (req, res) => {
+export const getCards = async (req: Request, res: Response) => {
   try {
     const data = await mtgClient.get('/cards');
     return res.status(200).send(data);
